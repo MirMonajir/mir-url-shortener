8d2823ba9b0f762c4071d133b5cee72f2edc6de0
MirMonajir <63101579+MirMonajir@users.noreply.github.com>
2025-10-16 07:32:42 +0530

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -11,7 +11,7 @@ func main() {
 //		log.Fatalf("Failed to load env file")}
 
 	r := SetupRouter()
-	if err = r.Run(":8080"); err != nil {
+	if err := r.Run(":8080"); err != nil {
 		log.Fatalf("Failed to start the URLShortener server: %v", err)
 	}
 }
