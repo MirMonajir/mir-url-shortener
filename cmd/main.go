2b14af9c2c72e1107c1ecb514ac9450ec8d417de
MirMonajir <63101579+MirMonajir@users.noreply.github.com>
2025-10-16 07:32:18 +0530

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -3,14 +3,12 @@ package main
 import (
 	"log"
 
-	"github.com/joho/godotenv"
+	//"github.com/joho/godotenv"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatalf("Failed to load env file")
-	}
+//	err := godotenv.Load()if err != nil {
+//		log.Fatalf("Failed to load env file")}
 
 	r := SetupRouter()
 	if err = r.Run(":8080"); err != nil {
